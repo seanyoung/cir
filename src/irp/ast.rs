69c90480b117395529bf695fea19d519c3ea0873
Sean Young <sean@mess.org>
2020-03-30 16:07:17 +0100

    Expression
    Signed-off-by: Sean Young <sean@mess.org>

--- src/irp/ast.rs
+++ src/irp/ast.rs
@@ -1,6 +1,7 @@
 pub struct Irp {
     pub general_spec: Vec<GeneralItem>,
     pub bit_spec: Vec<Vec<Duration>>,
+    pub stream: IrStream,
 }
 
 #[derive(PartialEq)]
@@ -21,9 +22,18 @@ pub enum GeneralItem {
 
 #[derive(PartialEq)]
 pub enum Duration {
-    Flash(f64, Unit),
-    Gap(f64, Unit),
-    Extent(f64, Unit),
+    FlashConstant(f64, Unit),
+    GapConstant(f64, Unit),
+    ExtentConstant(f64, Unit),
+    FlashIdentifier(String, Unit),
+    GapIdentifier(String, Unit),
+    ExtentIdentifier(String, Unit),
+}
+
+#[derive(PartialEq)]
+pub enum IrStreamItem {
+    Duration(Duration),
+    Assignment(String, Expression),
 }
 
 #[derive(PartialEq)]
@@ -36,6 +46,40 @@ pub enum RepeatMarker {
 
 #[derive(PartialEq)]
 pub struct IrStream {
-    pub stream: Vec<Duration>,
+    pub stream: Vec<IrStreamItem>,
     pub repeat: Option<RepeatMarker>,
+}
+
+#[derive(PartialEq)]
+pub enum Expression {
+    Number(i64),
+    Identifier(String),
+    Complement(Box<Expression>),
+    Not(Box<Expression>),
+    Negative(Box<Expression>),
+    BitCount(Box<Expression>),
+
+    Power(Box<Expression>, Box<Expression>),
+    Multiply(Box<Expression>, Box<Expression>),
+    Divide(Box<Expression>, Box<Expression>),
+    Modulo(Box<Expression>, Box<Expression>),
+    Add(Box<Expression>, Box<Expression>),
+    Subtract(Box<Expression>, Box<Expression>),
+
+    ShiftLeft(Box<Expression>, Box<Expression>),
+    ShiftRight(Box<Expression>, Box<Expression>),
+
+    LessEqual(Box<Expression>, Box<Expression>),
+    Less(Box<Expression>, Box<Expression>),
+    More(Box<Expression>, Box<Expression>),
+    MoreEqual(Box<Expression>, Box<Expression>),
+    Equal(Box<Expression>, Box<Expression>),
+    NotEqual(Box<Expression>, Box<Expression>),
+
+    BitwiseAnd(Box<Expression>, Box<Expression>),
+    BitwiseOr(Box<Expression>, Box<Expression>),
+    BitwiseXor(Box<Expression>, Box<Expression>),
+    Or(Box<Expression>, Box<Expression>),
+    And(Box<Expression>, Box<Expression>),
+    Ternary(Box<Expression>, Box<Expression>, Box<Expression>),
 }
