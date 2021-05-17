d64eb8186e645779e12c1bb29e75122ad99ea4c5
Sean Young <sean@mess.org>
2021-05-17 09:57:24 +0100

    Updates to rustdoc
    docs.rs does not like the intended code, see https://docs.rs/crate/irp/0.2.0
    and ADd some missing docs.
    
    Signed-off-by: Sean Young <sean@mess.org>

--- irp/src/lib.rs
+++ irp/src/lib.rs
@@ -14,27 +14,44 @@
 //!
 //! This example sets some parameters, encodes and then simply prints the result.
 //!
-//!     use irp::Irp;
-//!
-//!     let mut vars = irp::Vartable::new();
-//!     vars.set(String::from("D"), 255, 8);
-//!     vars.set(String::from("S"), 52, 8);
-//!     vars.set(String::from("F"), 1, 8);
-//!     let irp = Irp::parse("{38.4k,564}<1,-1|1,-3>(16,-8,D:8,S:8,F:8,~F:8,1,^108m,(16,-4,1,^108m)*) [D:0..255,S:0..255=255-D,F:0..255]")
-//!         .expect("parse should succeed");
-//!     let message = irp.encode(vars, 0).expect("encode should succeed");
-//!     if let Some(carrier) = &message.carrier {
-//!         println!("carrier: {}Hz", carrier);
-//!     }
-//!     if let Some(duty_cycle) = &message.duty_cycle {
-//!         println!("duty cycle: {}%", duty_cycle);
-//!     }
-//!     println!("{}", message.print_rawir());
+//! ```
+//! use irp::Irp;
+//
+//! let mut vars = irp::Vartable::new();
+//! vars.set(String::from("D"), 255, 8);
+//! vars.set(String::from("S"), 52, 8);
+//! vars.set(String::from("F"), 1, 8);
+//! let irp = Irp::parse(r#"
+//!     {38.4k,564}<1,-1|1,-3>(16,-8,D:8,S:8,F:8,~F:8,1,^108m,(16,-4,1,^108m)*)
+//!     [D:0..255,S:0..255=255-D,F:0..255]"#)
+//!     .expect("parse should succeed");
+//! let message = irp.encode(vars, 0).expect("encode should succeed");
+//! if let Some(carrier) = &message.carrier {
+//!     println!("carrier: {}Hz", carrier);
+//! }
+//! if let Some(duty_cycle) = &message.duty_cycle {
+//!     println!("duty cycle: {}%", duty_cycle);
+//! }
+//! println!("{}", message.print_rawir());
+//! ```
 //!
 //! The output is in raw ir format, which looks like "+9024 -4512 +564 -1692 +564 -1692 +564 -1692 +564 ...". The first
 //! entry in this array is *flash*, which means infrared light should be on for N microseconds, and every even entry
 //! means *gap*, which means absense of light, i.e. off, for N microseconds. This continues to alternate. The leading
 //! + and - also mean *flash* and *gap*.
+//!
+//! The IRP can also be encoded to pronto hex codes. Pronto hex codes have a repeating part, so no repeat argument is needed.
+//!
+//! ```
+//! use irp::Irp;
+//
+//! let mut vars = irp::Vartable::new();
+//! vars.set(String::from("F"), 1, 8);
+//! let irp = Irp::parse("{40k,600}<1,-1|2,-1>(4,-1,F:8,^45m)[F:0..255]")
+//!     .expect("parse should succeed");
+//! let pronto = irp.encode_pronto(vars).expect("encode should succeed");
+//! println!("pronto:{}", pronto);
+//! ```
 //!
 //! ## Parsing pronto hex codes
 //!
@@ -42,20 +59,22 @@
 //! Philips Pronto universal remote. The format is a series of 4 digits hex numbers. This library can parse the long
 //! codes, there is no support for the short format yet.
 //!
-//!     use irp::Pronto;
-//!
-//!     let pronto = Pronto::parse(r#"
-//!         0000 006C 0000 0022 00AD 00AD 0016 0041 0016 0041 0016 0041 0016 0016 0016
-//!         0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016
-//!         0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016
-//!         0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0041
-//!         0016 0041 0016 0041 0016 0041 0016 0041 0016 0041 0016 06FB
-//!         "#).expect("parse should succeed");
-//!     let message = pronto.encode(0);
-//!     if let Some(carrier) = &message.carrier {
-//!         println!("carrier: {}Hz", carrier);
-//!     }
-//!     println!("{}", message.print_rawir());
+//! ```
+//! use irp::Pronto;
+//
+//! let pronto = Pronto::parse(r#"
+//!     0000 006C 0000 0022 00AD 00AD 0016 0041 0016 0041 0016 0041 0016 0016 0016
+//!     0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0041 0016 0041 0016 0016
+//!     0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016
+//!     0016 0016 0016 0016 0016 0016 0016 0016 0016 0016 0041 0016 0016 0016 0041
+//!     0016 0041 0016 0041 0016 0041 0016 0041 0016 0041 0016 06FB
+//!     "#).expect("parse should succeed");
+//! let message = pronto.encode(0);
+//! if let Some(carrier) = &message.carrier {
+//!     println!("carrier: {}Hz", carrier);
+//! }
+//! println!("{}", message.print_rawir());
+//! ```
 //!
 //! ## Parsing lirc mode2 pulse space files
 //!
@@ -71,28 +90,31 @@
 //!
 //! This is an example of how to parse this. The result is printed in the more concise raw ir format.
 //!
-//!     let message = irp::mode2::parse(r#"
-//!         carrier 38400
-//!         pulse 9024
-//!         space 4512
-//!         pulse 4512
-//!     "#).expect("parse should succeed");
-//!     if let Some(carrier) = &message.carrier {
-//!         println!("carrier: {}Hz", carrier);
-//!     }
-//!     if let Some(duty_cycle) = &message.duty_cycle {
-//!         println!("duty cycle: {}%", duty_cycle);
-//!     }
-//!     println!("{}", message.print_rawir());
+//! ```
+//! let message = irp::mode2::parse(r#"
+//!     carrier 38400
+//!     pulse 9024
+//!     space 4512
+//!     pulse 4512
+//! "#).expect("parse should succeed");
+//! if let Some(carrier) = &message.carrier {
+//!     println!("carrier: {}Hz", carrier);
+//! }
+//! if let Some(duty_cycle) = &message.duty_cycle {
+//!     println!("duty cycle: {}%", duty_cycle);
+//! }
+//! println!("{}", message.print_rawir());
+//! ```
 //!
 //! ## Parsing raw ir format
 //!
 //! The raw ir format looks like "+100 -100 +100". The leading `+` and `-` may be omitted, but if present they are
 //! checked for consistency. The parse function returns a `Vec<u32>`.
 //!
-//!     let rawir: Vec<u32> = irp::rawir::parse("+100 -100 +100").expect("parse should succeed");
-//!     println!("{}", irp::rawir::print_to_string(&rawir));
-//!
+//! ```
+//! let rawir: Vec<u32> = irp::rawir::parse("+100 -100 +100").expect("parse should succeed");
+//! println!("{}", irp::rawir::print_to_string(&rawir));
+//! ```
 
 mod encode;
 pub mod mode2;
@@ -125,6 +147,7 @@ impl Message {
     }
 }
 
+/// A parsed or generated pronto hex code
 #[derive(Debug, PartialEq)]
 pub enum Pronto {
     LearnedUnmodulated {
@@ -139,6 +162,7 @@ pub enum Pronto {
     },
 }
 
+/// A parsed IRP notation, which can be used for encoding (and decoding in the future)
 pub struct Irp {
     general_spec: GeneralSpec,
     stream: Vec<Expression>,
