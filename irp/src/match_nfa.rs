fdcc82b83b56c9f46f2245f688987ec9492fb705
Sean Young <sean@mess.org>
2022-01-16 13:18:28 +0000

    Make nfa matcher work with incomplete flash/gap
    Signed-off-by: Sean Young <sean@mess.org>

--- irp/src/match_nfa.rs
+++ irp/src/match_nfa.rs
@@ -1,8 +1,7 @@
 use super::build_nfa::{Action, Edge, NFA};
 use super::Vartable;
-use std::collections::HashMap;
-#[allow(unused_imports)]
-use std::path::{Path, PathBuf};
+use crate::Expression;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct Matcher<'a> {
@@ -23,11 +22,47 @@ impl NFA {
     }
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub enum InfraredData {
     Flash(u32),
     Gap(u32),
     Reset,
+}
+
+impl InfraredData {
+    #[must_use]
+    fn consume(&self, v: u32) -> Self {
+        match self {
+            InfraredData::Flash(dur) => InfraredData::Flash(*dur - v),
+            InfraredData::Gap(dur) => InfraredData::Gap(*dur - v),
+            _ => unreachable!(),
+        }
+    }
+}
+
+impl fmt::Display for InfraredData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            InfraredData::Flash(dur) => write!(f, "+{}", dur),
+            InfraredData::Gap(dur) => write!(f, "-{}", dur),
+            InfraredData::Reset => write!(f, "!"),
+        }
+    }
+}
+
+impl<'a> fmt::Display for Vartable<'a> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut s = String::new();
+        for (name, (val, _, expr)) in &self.vars {
+            if let Some(expr) = expr {
+                s.push_str(&format!(" {} = {}", name, expr));
+            } else {
+                s.push_str(&format!(" {} = {}", name, val));
+            }
+        }
+
+        write!(f, "{}", s)
+    }
 }
 
 impl<'a> Matcher<'a> {
@@ -35,7 +70,7 @@ impl<'a> Matcher<'a> {
         self.pos.truncate(0);
     }
 
-    fn duration_matches(&self, expected: u32, received: u32) -> bool {
+    fn tolerance_eq(&self, expected: u32, received: u32) -> bool {
         let diff = if expected > received {
             expected - received
         } else {
@@ -59,139 +94,186 @@ impl<'a> Matcher<'a> {
             self.pos.push((0, Vartable::new()));
         }
 
-        let mut new_pos: HashMap<usize, Vartable> = HashMap::new();
+        let mut work = Vec::new();
 
-        let mut f = Vec::new();
+        for (pos, vartab) in &self.pos {
+            work.push((Some(ir), *pos, vartab.clone()));
+        }
 
-        std::mem::swap(&mut f, &mut self.pos);
+        let mut new_pos = Vec::new();
 
-        for (pos, vartab) in f.into_iter() {
+        while let Some((ir, pos, vartab)) = work.pop() {
             let edges = &self.nfa.verts[pos].edges;
 
+            //println!("pos:{} ir:{:?} vars:{}", pos, ir, vartab);
+
             for edge in edges {
+                //println!("edge:{:?}", edge);
+
                 match edge {
                     Edge::Flash(expected, dest) => {
-                        if let InfraredData::Flash(received) = ir {
-                            if self.duration_matches(*expected as u32, received) {
-                                new_pos.insert(*dest, vartab.clone());
+                        if let Some(ir @ InfraredData::Flash(received)) = ir {
+                            if self.tolerance_eq(*expected as u32, received) {
+                                let vartab = self.run_actions(pos, &vartab);
+
+                                work.push((None, *dest, vartab));
+                            } else if received > *expected as u32 {
+                                let vartab = self.run_actions(pos, &vartab);
+
+                                work.push((Some(ir.consume(*expected as u32)), *dest, vartab));
                             }
+                        } else if ir.is_none() && new_pos.iter().all(|(n, _)| *n != pos) {
+                            new_pos.push((pos, vartab.clone()));
                         }
                     }
                     Edge::Gap(expected, dest) => {
-                        if let InfraredData::Gap(received) = ir {
-                            if self.duration_matches(*expected as u32, received) {
-                                new_pos.insert(*dest, vartab.clone());
+                        if let Some(ir @ InfraredData::Gap(received)) = ir {
+                            if self.tolerance_eq(*expected as u32, received) {
+                                let vartab = self.run_actions(pos, &vartab);
+
+                                work.push((None, *dest, vartab));
+                            } else if received > *expected as u32 {
+                                let vartab = self.run_actions(pos, &vartab);
+
+                                work.push((Some(ir.consume(*expected as u32)), *dest, vartab));
                             }
+                        } else if ir.is_none() && new_pos.iter().all(|(n, _)| *n != pos) {
+                            new_pos.push((pos, vartab.clone()));
                         }
                     }
-                    _ => (),
+                    Edge::Branch(dest) => {
+                        let vartab = self.run_actions(pos, &vartab);
+
+                        work.push((ir, *dest, vartab));
+                    }
+                    Edge::BranchCond { expr, yes, no } => {
+                        let vartab = self.run_actions(pos, &vartab);
+
+                        let (cond, _) = expr.eval(&vartab).unwrap();
+
+                        let dest = if cond != 0 { *yes } else { *no };
+
+                        work.push((ir, dest, vartab));
+                    }
+                    Edge::Done => {
+                        let (val, _) = Expression::Identifier(String::from("$bits"))
+                            .eval(&vartab)
+                            .unwrap();
+                        self.reset();
+
+                        return Some(val as u64);
+                    }
                 }
             }
         }
 
-        let mut changes;
-
-        for (mut pos, mut vartable) in new_pos {
-            changes = true;
-
-            while changes {
-                changes = false;
-                for a in &self.nfa.verts[pos].actions {
-                    match a {
-                        Action::Set { var, expr } => {
-                            let (val, len) = expr.eval(&vartable).unwrap();
-                            vartable.vars.insert(var.to_string(), (val, len, None));
-                            changes = true;
-                        }
-                    }
-                }
-
-                for edge in &self.nfa.verts[pos].edges {
-                    match edge {
-                        Edge::Branch(dest) => {
-                            changes = true;
-                            pos = *dest;
-                        }
-                        Edge::BranchCond { expr, yes, no } => {
-                            let (cond, _) = expr.eval(&vartable).unwrap();
-
-                            pos = if cond != 0 { *yes } else { *no };
-                            changes = true;
-                        }
-                        Edge::Done => {
-                            let (val, _) = Expression::Identifier(String::from("$bits"))
-                                .eval(&vartable)
-                                .unwrap();
-                            self.reset();
-
-                            return Some(val as u64);
-                        }
-                        _ => (),
-                    }
-                }
-            }
-
-            self.pos.push((pos, vartable));
-        }
+        self.pos = new_pos;
 
         None
     }
+
+    fn run_actions<'v>(&self, pos: usize, vartab: &Vartable<'v>) -> Vartable<'v> {
+        let mut vartable = vartab.clone();
+
+        for a in &self.nfa.verts[pos].actions {
+            match a {
+                Action::Set { var, expr } => {
+                    let (val, len) = expr.eval(&vartable).unwrap();
+                    vartable.vars.insert(var.to_string(), (val, len, None));
+                }
+            }
+        }
+
+        vartable
+    }
 }
 
-use crate::Expression;
 #[cfg(test)]
-use crate::Irp;
+mod test {
+    use num::Integer;
 
-#[test]
-fn sony8() {
-    // sony 8
-    let irp = Irp::parse("{40k,600}<1,-1|2,-1>(4,-1,F:8,^45m)[F:0..255]").unwrap();
+    use super::{InfraredData, Matcher};
+    use crate::{rawir, Irp};
+    use std::path::PathBuf;
 
-    let nfa = irp.build_nfa().unwrap();
+    fn munge(matcher: &mut Matcher, s: &str) -> Option<u64> {
+        let mut res = None;
 
-    let mut matcher = nfa.matcher(100, 3);
+        for ir in rawir::parse(s)
+            .unwrap()
+            .iter()
+            .enumerate()
+            .map(|(no, len)| {
+                if no.is_odd() {
+                    InfraredData::Gap(*len)
+                } else {
+                    InfraredData::Flash(*len)
+                }
+            })
+        {
+            if let Some(r) = matcher.input(ir) {
+                if res.is_some() {
+                    panic!("double result: {:?} and {:?}", res, r);
+                }
 
-    let mut res = None;
-
-    for ir in vec![
-        InfraredData::Flash(2400),
-        InfraredData::Gap(600),
-        InfraredData::Flash(600),
-        InfraredData::Gap(600),
-        InfraredData::Flash(600),
-        InfraredData::Gap(600),
-        InfraredData::Flash(1200),
-        InfraredData::Gap(600),
-        InfraredData::Flash(600),
-        InfraredData::Gap(600),
-        InfraredData::Flash(600),
-        InfraredData::Gap(600),
-        InfraredData::Flash(600),
-        InfraredData::Gap(600),
-        InfraredData::Flash(1200),
-        InfraredData::Gap(600),
-        InfraredData::Flash(1200),
-        InfraredData::Gap(600),
-    ] {
-        if let Some(r) = matcher.input(ir) {
-            if res.is_some() {
-                panic!("double result: {:?} and {:?}", res, r);
+                res = Some(r);
             }
-
-            res = Some(r);
         }
+
+        res
     }
 
-    assert_eq!(res, Some(196));
-}
+    #[test]
+    fn sony8() {
+        // sony 8
+        let irp = Irp::parse("{40k,600}<1,-1|2,-1>(4,-1,F:8,^45m)[F:0..255]").unwrap();
 
-#[test]
-fn nec() {
-    let irp = Irp::parse("{38.4k,564}<1,-1|1,-3>(16,-8,D:8,S:8,F:8,~F:8,1,^108m,(16,-4,1,^108m)*) [D:0..255,S:0..255=255-D,F:0..255]").unwrap();
+        let nfa = irp.build_nfa().unwrap();
 
-    let nfa = irp.build_nfa().unwrap();
+        let mut matcher = nfa.matcher(100, 3);
 
-    nfa.dotgraphviz(&PathBuf::from("test.dot"));
+        let  res = munge(&mut matcher,
+            "+2400 -600 +600 -600 +600 -600 +1200 -600 +600 -600 +600 -600 +600 -600 +1200 -600 +1200 -31200");
 
-    let _matcher = nfa.matcher(100, 3);
+        assert_eq!(res, Some(196));
+    }
+
+    #[test]
+    fn nec() {
+        let irp = Irp::parse("{38.4k,564}<1,-1|1,-3>(16,-8,D:8,S:8,F:8,~F:8,1,^108m,(16,-4,1,^108m)*) [D:0..255,S:0..255=255-D,F:0..255]").unwrap();
+
+        let nfa = irp.build_nfa().unwrap();
+
+        let mut matcher = nfa.matcher(100, 3);
+
+        // let res = munge(&mut matcher, "+9024 -2256 +564 -96156");
+
+        // assert_eq!(res, Some(196));
+
+        // matcher.reset();
+
+        let res = munge(&mut matcher,
+            "+9024 -4512 +564 -564 +564 -564 +564 -564 +564 -564 +564 -564 +564 -564 +564 -1692 +564 -564 +564 -1692 +564 -1692 +564 -1692 +564 -1692 +564 -1692 +564 -1692 +564 -564 +564 -1692 +564 -564 +564 -564 +564 -1692 +564 -564 +564 -564 +564 -564 +564 -1692 +564 -1692 +564 -1692 +564 -1692 +564 -564 +564 -1692 +564 -1692 +564 -1692 +564 -564 +564 -564 +564 -39756");
+
+        // not quite
+        assert_eq!(res, Some(1002749760));
+    }
+
+    #[test]
+    #[ignore] // broken
+    fn rc5() {
+        // RC5
+        let irp = Irp::parse("{36k,msb,889}<1,-1|-1,1>((1,~F:1:6,T:1,D:5,F:6,^114m)*,T=1-T)[D:0..31,F:0..127,T@:0..1=0]").unwrap();
+
+        let nfa = irp.build_nfa().unwrap();
+
+        nfa.dotgraphviz(&PathBuf::from("test.dot"));
+
+        let mut matcher = nfa.matcher(100, 3);
+
+        let  res = munge(&mut matcher,
+            "+889 -889 +1778 -1778 +889 -889 +889 -889 +889 -889 +1778 -889 +889 -889 +889 -889 +889 -889 +889 -889 +889 -1778 +889 -89997");
+
+        assert_eq!(res, Some(196));
+    }
 }
