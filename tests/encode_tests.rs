d2b1dab1d2d85a0b616be8882ae0700df3c5f9f8
Sean Young <sean@mess.org>
2022-02-13 11:14:48 +0000

    Add lircd encode test
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/encode_tests.rs
+++ tests/encode_tests.rs
@@ -28,6 +28,36 @@ rawir: +2400 -600 +600 -600 +600 -600 +1200 -600 +1200 -600 +600 -600 +600 -600 
     );
 }
 
+#[test]
+fn encode_lircd_raw_test() {
+    let mut cmd = Command::cargo_bin("cir").unwrap();
+
+    let assert = cmd
+        .args(&[
+            "encode",
+            "lircd",
+            "testdata/lircd_conf/pace/DC420N.lircd.conf",
+            "Foxtel_Digital_Cable_STB",
+            "1",
+        ])
+        .assert();
+
+    let output = assert.get_output();
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    assert_eq!(stderr, "");
+
+    assert_eq!(
+        stdout,
+        r#"carrier: 36000Hz
+duty cycle: 50%
+rawir: +2664 -888 +444 -444 +444 -444 +444 -888 +444 -888 +888 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +444 -444 +888 -123496
+"#
+    );
+}
+
 ///
 /// This tests needs a /dev/lirc0 rc-loopback device
 #[test]
