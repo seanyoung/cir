f9cdd8f04015a8fbfdece7c09851e7e1ce283aa6
Sean Young <sean@mess.org>
2022-02-19 11:31:17 +0000

    If remote has repeat parameter, discard any second scancode
    Signed-off-by: Sean Young <sean@mess.org>

--- tests/lircd_conf_encode.rs
+++ tests/lircd_conf_encode.rs
@@ -122,14 +122,24 @@ fn lircd_encode(conf: &Path, testdata: &Path, log: &Log) {
 
                 let mut message = Message::new();
 
-                for code in &code.code {
+                if code.code.len() == 2 && remote.repeat.0 != 0 && remote.repeat.1 != 0 {
+                    // if remote has a repeat parameter and two scancodes, then just repeat the first scancode
                     let mut vars = Vartable::new();
-                    vars.set(String::from("CODE"), *code as i64, 32);
+                    vars.set(String::from("CODE"), code.code[0] as i64, 32);
 
-                    // FIXME: should be possible to test repeats
-                    let m = irp.encode(vars, 0).expect("encode should succeed");
+                    let m = irp.encode(vars, 1).expect("encode should succeed");
 
                     message.extend(&m);
+                } else {
+                    for code in &code.code {
+                        let mut vars = Vartable::new();
+                        vars.set(String::from("CODE"), *code as i64, 32);
+
+                        // FIXME: should be possible to test repeats
+                        let m = irp.encode(vars, 0).expect("encode should succeed");
+
+                        message.extend(&m);
+                    }
                 }
 
                 if message.raw.len().is_even() {
